27f1a1e138e38c5c17700e349ce6175bf6ff1157
hack <hack>
2020-12-17 18:54:24 -0800

    Structured English

---
+++ IS51Final.py
@@ -0,0 +1,9 @@
+"""In the main function I open the Final.txt file and then I get all the values on each line
+which are the grades and put them into a list. They are all strings so i iterate through 
+the list convert the individual grades in strings to integers and put them into another filke.
+Then I add up the numbers in integers list to get the overall sum and divide by the length of the list
+to get the average grade.
+
+In the calculate_percent_above_average I take the average and the list of integers and I do comparisons 
+to find out the total number of people with above average grades and divide that as a percentage of the total 
+number of grades and round to find the number of people with above average grades."""
